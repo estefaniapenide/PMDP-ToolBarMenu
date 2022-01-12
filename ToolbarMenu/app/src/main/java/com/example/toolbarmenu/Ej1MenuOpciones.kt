133e946e8eebba4c4a5249ad6cec55a2fafaf5ce
etef <estefaniapenide@gmail.com>
2022-01-12 21:14:37 +0100

    Primer comint

--- ToolbarMenu/app/src/main/java/com/example/toolbarmenu/Ej1MenuOpciones.kt
+++ ToolbarMenu/app/src/main/java/com/example/toolbarmenu/Ej1MenuOpciones.kt
@@ -2,10 +2,19 @@ package com.example.toolbarmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class Ej1MenuOpciones : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ej1_menu_opciones)
+
+        supportActionBar!!.title="TOOLBAR"
+        supportActionBar!!.subtitle="toolbar"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu1,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
